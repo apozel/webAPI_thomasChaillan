7265f940a5aedca53eee5a509255c0dea4a3150f
apozel <52205094+apozel@users.noreply.github.com>
2019-11-19 09:36:21 +0100

    version avec la autocompletion

--- FilmPlastic/src/app/recherche/recherche.component.ts
+++ FilmPlastic/src/app/recherche/recherche.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { FormControl } from '@angular/forms';
 import { Observable } from 'rxjs';
-import { startWith, map } from 'rxjs/operators';
+import { startWith, map, tap } from 'rxjs/operators';
 import { Film } from '../Film';
 
 @Component({
@@ -19,11 +19,16 @@ export class RechercheComponent implements OnInit {
     this.filteredOptions = this.myControl.valueChanges
       .pipe(
         startWith(''),
+        tap(value => console.log(value)),
         map(value => typeof value === 'string' ? value : value.Title),
         map(Title => Title ? this._filter(Title) : this.options.slice())
 
       );
-    console.log(this.filteredOptions);
+
+  }
+
+  montreMoiSaValeur() {
+
   }
 
   displayFn(film?: Film): string | undefined {
@@ -32,7 +37,6 @@ export class RechercheComponent implements OnInit {
 
   private _filter(Title: string): Film[] {
     const filterValue = Title.toLowerCase();
-
     return this.options.filter(option => option.Title.toLowerCase().indexOf(filterValue) === 0);
   }
 }
